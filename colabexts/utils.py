e6fedab5d2f8c642a738d8d7eae853d3fdf172d5
meyers007 <sada.narayanappa@gmail.com>
2024-04-23 09:23:28 -0600

    Commiting

--- colabexts/utils.py
+++ colabexts/utils.py
@@ -1,32 +1,8 @@
 import os, sys, re, importlib, json, datetime, glob, platform, matplotlib, dateutil 
 import numpy as np
-from IPython.display import HTML, Javascript
-import pandas as pd
 from datetime import timedelta;
-from collections import defaultdict
-from pylab import rcParams
-import matplotlib.pyplot as plt
 import urllib.request;
-from random import randint
-from pylab import rcParams
-import warnings
-warnings.filterwarnings('ignore')
-
-matplotlib.rcParams['figure.figsize'] = (16, 5)
-matplotlib.rcParams['axes.grid'] = True
-
-pd.set_option('display.max_colwidth', 1024)
-pd.set_option('display.max_rows', 6)
-
-params = {'legend.fontsize': 'small',
-          'figure.figsize': (16, 5),
-          'axes.titlesize':'medium',
-         'axes.labelsize': 'x-small',
-         'xtick.labelsize':'x-small',
-         'ytick.labelsize':'x-small' }
-#matplotlib.pyplot.rcParams.update(params)
-import colabexts
-
+from collections import defaultdict
 #-----------------------------------------------------------------------------------
 class Map(dict):
     """
@@ -41,7 +17,7 @@ class Map(dict):
                     self[k] = v
 
         if kwargs:
-            for k, v in kwargs.iteritems():
+            for k, v in kwargs.items():
                 self[k] = v
 
     def __getattr__(self, attr):
@@ -61,49 +37,25 @@ class Map(dict):
         super(Map, self).__delitem__(key)
         del self.__dict__[key]
 #-----------------------------------------------------------------------------------
-def pr():
-    p='''
-import os, sys, re, importlib, json, datetime, glob, platform, matplotlib, dateutil, warnings
-import numpy as np
-from IPython.display import HTML, Javascript
-import pandas as pd
-from datetime import timedelta;
-from collections import defaultdict
-import matplotlib.pyplot as plt
-import urllib.request;
-from random import randint
-from collections import defaultdict
-from pylab import rcParams
-rcParams['figure.figsize'] = 13, 5
-warnings.filterwarnings('ignore')
-%matplotlib inline
-'''
-    print(p)
+class mydict(dict):
+    def __init__(self, default_type="|key|locals|globals", *args, **kwargs):
+        super(mydict, self).__init__(*args, **kwargs)
+        for arg in args:
+            if isinstance(arg, dict):
+                for k, v in arg.items():
+                    self[k] = v
+        if kwargs:
+            for k, v in kwargs.items():
+                self[k] = v
 
-def loadExtensions(abspath=None, debug=False):
-    ip = get_ipython()
-    if(abspath is None):
-        abspath = os.path.dirname(colabexts.__file__)
-    if(debug):
-        print("#loading colabexts ... from: "+abspath);
-        pr();
+    def __missing__(self, key):
+        if key in  locals():
+            return locals()[key]
+        return key
         
-    ip.run_line_magic(magic_name="run", line=abspath+"/jcommon.ipynb")
-    
-#-----------------------------------------------------------------------------------
-def readFile(file):
-    with open(file, "rb") as f:
-        c = f.read().decode().replace('\r\n', '\n')
-        return c;
-#-----------------------------------------------------------------------------------
-def jlog(*args, debug=False, end=' ', **kwargs):
-    if (not debug or 'debug' in kwargs and not kwargs(debug) ):
-        return;
+def parsej(s, *args, **kwargs):
+    return eval(s, mydict(*args, **kwargs))
 
-    for a in args:
-        print(a, end=end)
-    for k,v in kwargs.items():
-        print ("%s = %s" % (k, v))
 #-----------------------------------------------------------------------------------
 def inJupyter():
     try:    get_ipython; return True
@@ -127,7 +79,6 @@ def encrypt(msg_text = b'message', secret_key='password'):
     ret = encoded.decode("utf-8")
     print(ret)
     return ret
-
 #--------------------------------------------------------------------------------
 def decrypt(encoded, secret_key='password'):
     if (type(secret_key) == str):
@@ -140,4 +91,7 @@ def decrypt(encoded, secret_key='password'):
     ret =decoded.decode("utf-8").strip()
     print(ret)
     return ret
+#--------------------------------------------------------------------------------
+def parse(s="{}", {}):
+    pass
     
